543ef11697827519e0854daca12985ea2e57cfe3
Tyson Hancock <tyson@Tysons-MacBook-Pro.local>
2019-01-08 14:50:23 -0700

    finished first problems

--- practice.js
+++ practice.js
@@ -18,7 +18,7 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){return arr[0]}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +33,7 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){return arr[2]}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +48,10 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  for(i=0; i<family.length; i++){
+    alert(family[i]);}
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +66,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters)
+{for(i=letters.length-1; i>=0; i--){
+  alert(letters[i])}}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +83,17 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(nums){
+  var array=[];
+  for(i=0; i<nums.length; i++){
+    if(nums[i]%2 ===0){
+      array.push(nums[i]);
+    }
+  }
+  return array
+}
+  
+      
 
 
 
